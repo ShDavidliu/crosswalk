58dcb548081459421d1009b24c123b6aa094219f
David Liu <david.liu@intel.com>
2014-02-25 13:37:55 +0800

    update

--- app/tools/android/make_apk_test.py
+++ app/tools/android/make_apk_test.py
@@ -154,14 +154,6 @@ class TestMakeApk(unittest.TestCase):
            '--package=org.xwalk.example', self._mode]
     out = RunCommand(cmd)
     self.assertTrue(out.find('The APK name is required!') != -1)
-<<<<<<< HEAD
-    Clean('Example')
-    cmd = ['python', 'make_apk.py', '--name=Example', '--app-version=1.0.0',
-           '--package=org.xwalk.example', self._mode]
-    out = RunCommand(cmd)
-    self.assertTrue(out.find('The APK name is required!') == -1)
-    Clean('Example')
-=======
     Clean('Example', '1.0.0')
     cmd = ['python', 'make_apk.py', '--name="Test Example"',
            '--app-version=1.0.0',
@@ -169,7 +161,6 @@ class TestMakeApk(unittest.TestCase):
     out = RunCommand(cmd)
     self.assertTrue(out.find('The APK name is required!') == -1)
     Clean('Test Example', '1.0.0')
->>>>>>> ecc837b... [Android] Support package file name including space character.
     # The following invalid chars verification is too heavy for embedded mode,
     # and the result of verification should be the same between shared mode
     # and embedded mode. So only do the verification in the shared mode.
